0f08afb602684ba8bb635ada22e7e6ef4758de4c
Leon Samson Vestby <leon.vestby@alf.no>
2025-05-30 13:54:53 +0200

    oppg_14

---
+++ J14/spelarar.js
@@ -0,0 +1,221 @@
+let lag = [
+            {
+                Nummer: 1,
+                Posisjon: "Keeper",
+                Fornavn: "Mathias",
+                Etternavn: "Dyngeland",
+                Høyde: 184,
+                Nasjonalitet: "Norge",
+                Født: "07-10-1995"
+            },
+            {
+                Bilete: "64px-Dice-1-b.svg.png",
+                Nummer: 12,
+                Posisjon: "Keeper",
+                Fornavn: "Martin",
+                Etternavn: "Børsheim",
+                Nasjonalitet: "Norge",
+                Født: "18-02-2005"
+            },
+            {
+                Nummer: 24,
+                Posisjon: "Keeper",
+                Fornavn: "Mathias",
+                Etternavn: "Klausen",
+                Nasjonalitet: "Norge",
+                Født: "10-10-2007"
+            },
+            {
+                Nummer: 21,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Denzel",
+                Etternavn: "De Roeve",
+                Nasjonalitet: "Belgia",
+                Født: "10-08-2004"
+            },
+            {
+                Nummer: 38,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Patrik",
+                Etternavn: "Andersen",
+                Nasjonalitet: "Norge",
+                Født: "30-01-2005"
+            },
+            {
+                Nummer: 2,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Martin",
+                Etternavn: "Hellan",
+                Nasjonalitet: "Norge",
+                Født: "17- 08-2003"
+            },
+            {
+                Nummer: 3,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Fredrik",
+                Etternavn: "Pallesen Knudsen",
+                Nasjonalitet: "Norge",
+                Født: "30-08-1996"
+            },
+            {
+                Nummer: 17,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Joachim",
+                Etternavn: "Soltvedt",
+                Nasjonalitet: "Norge",
+                Født: "09-09-1995"
+            },
+            {
+                Nummer: 6,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Japhet",
+                Etternavn: "Sery Larsen",
+                Nasjonalitet: "Danmark",
+                Født: "10-04-2000"
+            },
+            {
+                Nummer: 15,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Jonas",
+                Etternavn: "Torsvik",
+                Nasjonalitet: "Norge",
+                Født: "24-05-2005"
+            },
+            {
+                Nummer: 23, // her
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Thore",
+                Etternavn: "Pedersen",
+                Nasjonalitet: "Norge",
+                Født: "11-08-1996"
+            },
+            {
+                Nummer: 26,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Eivind",
+                Etternavn: "Helland",
+                Nasjonalitet: "Norge",
+                Født: "25-04-2005"
+            },
+            {
+                Nummer: 43,
+                Posisjon: "Forsvarsspiller",
+                Fornavn: "Rasmus",
+                Etternavn: "Holten",
+                Nasjonalitet: "Norge",
+                Født: "20-02-2005"
+            },
+            {
+                Nummer: 5,
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Sakarias",
+                Etternavn: "Opsahl",
+                Nasjonalitet: "Norge",
+                Født: "17-07-1999"
+            },
+            {
+                Nummer: 8,
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Felix",
+                Etternavn: "Horn Myhre",
+                Nasjonalitet: "Norge",
+                Født: "04-03-1999"
+            },
+            {
+                Nummer: 10,
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Emil",
+                Etternavn: "Kornvig",
+                Nasjonalitet: "Norge",
+                Født: "28-04-2000"
+            },
+            {
+                Nummer: 14, // her
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Ulrik",
+                Etternavn: "Mathisen",
+                Nasjonalitet: "Norge",
+                Født: "18-01-1999"
+            },
+            {
+                Nummer: 19,
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Eggert Aron",
+                Etternavn: "Gudmundsson",
+                Nasjonalitet: "Island",
+                Født: "08-02-2004"
+            },
+            {
+                Nummer: 25,
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Niklas",
+                Etternavn: "Wassberg",
+                Nasjonalitet: "Norge",
+                Født: "12-06-2004"
+            },
+            {
+                Nummer: 27,
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Mads",
+                Etternavn: "Sande",
+                Nasjonalitet: "Norge",
+                Født: "22-03-1998"
+            },
+            {
+                Nummer: 41,
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Lars",
+                Etternavn: "Remmem",
+                Nasjonalitet: "Norge",
+                Født: "18-2-2006"
+            },
+            {
+                Nummer: 32, // her
+                Posisjon: "Midtbanespiller",
+                Fornavn: "Markus",
+                Etternavn: "Haaland",
+                Nasjonalitet: "Norge",
+                Født: "08-03-2005"
+            },
+            {
+                Nummer: 37,
+                Posisjon: "Angrepsspiller",
+                Fornavn: "Jon",
+                Etternavn: "Berisha",
+                Nasjonalitet: "Norge",
+                Født: "21-01-2005"
+            },
+            {
+                Nummer: 7,
+                Posisjon: "Angrepsspiller",
+                Fornavn: "Mads",
+                Etternavn: "Hansen",
+                Nasjonalitet: "Danmark",
+                Født: "28-07-2002"
+            },
+            {
+                Nummer: 9,
+                Posisjon: "Angrepsspiller",
+                Fornavn: "Niklas",
+                Etternavn: "Castro",
+                Nasjonalitet: "Chile",
+                Født: "08-01-1996"
+            },
+            {
+                Nummer: 11,
+                Posisjon: "Angrepsspiller",
+                Fornavn: "Bård",
+                Etternavn: "Finne",
+                Nasjonalitet: "Norge",
+                Født: "13-02-1995"
+            },
+            {
+                Nummer: 20,
+                Posisjon: "Angrepsspiller",
+                Fornavn: "Aune",
+                Etternavn: "Heggebø",
+                Nasjonalitet: "Norge",
+                Født: "29-07-2001"
+            }
+
+        ]
